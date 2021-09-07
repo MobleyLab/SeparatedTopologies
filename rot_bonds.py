da71e2c598d37ce9351f0b8b3b09eb6c1a53d93d
hannahbaumann <hbaumann@uci.edu>
2021-09-07 15:04:22 -0700

    Load in mol2 of ligand instead of pdb of complex to find rot bonds.

--- rot_bonds.py
+++ rot_bonds.py
@@ -7,78 +7,70 @@ from openeye.oedepict import *
 def rota_bonds(file):
     ''' Find rotatable bonds ligand. Check for symmetry and only include non-symmetric rotatable bonds '''
 
-    complex_A = oechem.OEGraphMol()
-    ifs = oechem.oemolistream()
-    ifs.SetFlavor(oechem.OEFormat_PDB,
-                  oechem.OEIFlavor_PDB_Default | oechem.OEIFlavor_PDB_DATA | oechem.OEIFlavor_PDB_ALTLOC)
-    ifs.open(file)
-    oechem.OEReadMolecule(ifs, complex_A)
-    ifs.close()
-
+    #Load in ligand from mol2 into openeye
+    ifs = oechem.oemolistream(file)
     lig = oechem.OEGraphMol()
-    prot = oechem.OEGraphMol()
-    wat = oechem.OEGraphMol()
-    other = oechem.OEGraphMol()
-    #get ligand
-    if oechem.OESplitMolComplex(lig, prot, wat, other, complex_A):
-        rot_bonds = []
-        OEPerceiveSymmetry(lig)
+    oechem.OEReadMolecule(ifs, lig)
 
-        for atom in lig.GetAtoms():
-            for bond in atom.GetBonds():
-                rot_bond = []
-                #check if bond is rotatable
-                if bond.IsRotor():
-                    #find neighboring atom (atom2)
-                    nbor = bond.GetNbr(atom)
-                    #only count bond once (we also loop over neighboring atom, same bond)
-                    if nbor.GetIdx() > atom.GetIdx():
-                        symmetry = False
-                        # loop over all atoms that atom1 is connected with, get their symmetry class
-                        sym_a1 = []
+    rot_bonds = []
+    OEPerceiveSymmetry(lig)
+
+    for atom in lig.GetAtoms():
+        for bond in atom.GetBonds():
+            rot_bond = []
+            # check if bond is rotatable
+            if bond.IsRotor():
+
+                # find neighboring atom (atom2)
+                nbor = bond.GetNbr(atom)
+                # only count bond once (we also loop over neighboring atom, same bond)
+                if nbor.GetIdx() > atom.GetIdx():
+                    symmetry = False
+                    # loop over all atoms that atom1 is connected with, get their symmetry class
+                    sym_a1 = []
+                    for a in atom.GetAtoms():
+                        sym = a.GetSymmetryClass()
+                        # if the same symmetry group appears more than once: there is symmetry
+                        if sym in sym_a1:
+                            symmetry = True
+                        sym_a1.append(sym)
+                    # loop over all atoms that atom2 is connected with, get their symmetry class
+                    # do this since symmetry can occur at either side of the rotatable bond
+                    sym_a2 = []
+                    for a in nbor.GetAtoms():
+                        sym = a.GetSymmetryClass()
+                        if sym in sym_a2:
+                            symmetry = True
+                        sym_a2.append(sym)
+                    # if the rotatable bond is not symmetric: save the 4 atoms of that dihedral
+                    if symmetry == False:
                         for a in atom.GetAtoms():
-                            sym = a.GetSymmetryClass()
-                            #if the same symmetry group appears more than once: there is symmetry
-                            if sym in sym_a1:
-                                symmetry = True
-                            sym_a1.append(sym)
-                        # loop over all atoms that atom2 is connected with, get their symmetry class
-                        # do this since symmetry can occur at either side of the rotatable bond
-                        sym_a2 = []
-                        for a in nbor.GetAtoms():
-                            sym = a.GetSymmetryClass()
-                            if sym in sym_a2:
-                                symmetry = True
-                            sym_a2.append(sym)
-                        # if the rotatable bond is not symmetric: save the 4 atoms of that dihedral
-                        if symmetry == False:
-                            for a in atom.GetAtoms():
-                                if a.GetIdx() != nbor.GetIdx() and a.IsHydrogen() == False:
-                                    d1 = a.GetIdx()
+                            if a.GetIdx() != nbor.GetIdx() and a.IsHydrogen() == False:
+                                d1 = a.GetIdx()
 
-                            for n in nbor.GetAtoms():
-                                if n.GetIdx() != atom.GetIdx() and n.IsHydrogen() == False:
-                                    d4 = n.GetIdx()
-                            rot_bond.append(d1)
-                            rot_bond.append(atom.GetIdx())
-                            rot_bond.append(nbor.GetIdx())
-                            rot_bond.append(d4)
-                            rot_bonds.append(rot_bond)
+                        for n in nbor.GetAtoms():
+                            if n.GetIdx() != atom.GetIdx() and n.IsHydrogen() == False:
+                                d4 = n.GetIdx()
+                        rot_bond.append(d1)
+                        rot_bond.append(atom.GetIdx())
+                        rot_bond.append(nbor.GetIdx())
+                        rot_bond.append(d4)
+                        rot_bonds.append(rot_bond)
 
     return rot_bonds
 
 
-def get_dihedrals(file, lig):
+def get_dihedrals(ligand, file, lig):
     '''Get dihedral around rotatable bond'''
     traj = md.load(file)
     topology = traj.topology
-    ligand = topology.select('resname %s' % lig).tolist()
-    len_lig = len(ligand)
-    rot_bonds = rota_bonds(file)
+    ligand_top = topology.select('resname %s' % lig).tolist()
+    len_lig = len(ligand_top)
+    rot_bonds = rota_bonds(ligand)
     dih = []
     values = []
     for rb in rot_bonds:
-        rb = [ligand[r] for r in rb]
+        rb = [ligand_top[r] for r in rb]
         dih1 = np.rad2deg(md.compute_dihedrals(traj, [np.array(rb)]))
         dih.append([r + 1 for r in rb])
         values.append(round(float(dih1[0]), 2))
@@ -112,9 +104,25 @@ def write_itp_restraints(dih, values, forceconst_A, forceconst_B, file):
 
     return
 
-def restrain_rot_bonds(fileA, fileB, folder,lig = 'MOL'):
-    dih_A, values_A, len_ligA = get_dihedrals(fileA, lig)
-    dih, values_B, len_lig = get_dihedrals(fileB, lig)
+def restrain_rot_bonds(ligand_A, ligand_B, fileA, fileB, folder,lig = 'MOL'):
+    """Restrain rotatable bonds.
+        Parameters
+        ----------
+        ligand_A: str
+            Mol2 file for ligand A
+        ligand_B: str
+            Mol2 file for ligand B
+        fileA: str
+            coordinate file solvated protein-ligand system ligand A
+        fileB: str
+            coordinate file solvated protein-ligand system ligand B
+        folder: str
+            path for output .itp files
+        lig: str
+            Three letter code for ligand
+        """
+    dih_A, values_A, len_ligA = get_dihedrals(ligand_A, fileA, lig)
+    dih, values_B, len_lig = get_dihedrals(ligand_B, fileB, lig)
     dih_B = []
     for d in dih:
         d = [x + len_ligA for x in d]
